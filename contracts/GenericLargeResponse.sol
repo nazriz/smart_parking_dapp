ee370ded3ea15b0e4b2bb930c1e4c39a04670ec7
Nazim Rizvic <nazim.rizvic@me.com>
2022-07-30 11:42:22 +1000

    fixed mapping to use the address of true msg.sender, and not chainlink oracle. Changed to bytes32 from string.

--- contracts/GenericLargeResponse.sol
+++ contracts/GenericLargeResponse.sol
@@ -10,7 +10,8 @@ contract GenericLargeResponse is ChainlinkClient, ConfirmedOwner {
 
     // variable bytes(arbitrary-length raw byte data) returned in a single oracle response
     bytes public data;
-    string public parking_spot_location;
+    bytes32 public parking_spot_location;
+    address private parkingSpotRequesterAddr;
 
     bytes32 private jobId;
     uint256 private fee;
@@ -22,14 +23,17 @@ contract GenericLargeResponse is ChainlinkClient, ConfirmedOwner {
         fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
     }
 
+    mapping(address=>bytes32[]) public parkingSpotLocationOwnerArray;
+    mapping(address=>bytes32) public parkingSpotLocationOwner;
+
 
     function requestOffchainParkingSpotData() public {
+        parkingSpotRequesterAddr = msg.sender;
         string memory sender_address = Strings.toHexString(uint160(msg.sender), 20);
         Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfillBytes.selector);
         string memory api_url = "https://yfkocaqzu8.execute-api.us-east-1.amazonaws.com/test/register?address=";
         string memory url = string.concat(api_url,sender_address);
         req.add("get", url );
-
         req.add("path","location"); // Chainlink nodes 1.0.0 and later support this format
         sendChainlinkRequest(req, fee);
     }
@@ -40,7 +44,11 @@ contract GenericLargeResponse is ChainlinkClient, ConfirmedOwner {
     function fulfillBytes(bytes32 requestId, bytes memory bytesData) public recordChainlinkFulfillment(requestId) {
         emit RequestFulfilled(requestId, bytesData);
         data = bytesData;
-        parking_spot_location = string(data);
+        parking_spot_location = bytes32(data);
+        parkingSpotLocationOwner[parkingSpotRequesterAddr] = bytes32(data);
+        parkingSpotLocationOwnerArray[parkingSpotRequesterAddr].push(bytes32(data));
+
+
     }
 
 
@@ -48,6 +56,15 @@ contract GenericLargeResponse is ChainlinkClient, ConfirmedOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
         require(link.transfer(msg.sender, link.balanceOf(address(this))), 'Unable to transfer');
     }
+
+    function checkParkingSpotLocationOwner(address _address) public view returns ( bytes32) {
+        return parkingSpotLocationOwner[_address];
+    }
+
+       function checkParkingSpotLocationOwnerArray(address _address, uint16 _index) public view returns ( bytes32) {
+        return parkingSpotLocationOwnerArray[_address][_index];
+    }
+
 }
 
 
