e1748d30986e18aee496097c829c8a92b5f249e6
Nazim Rizvic <nazim.rizvic@me.com>
2022-08-23 12:49:43 +1000

    supports basic depositing and requesting functionality - checks for min deposit and token availability

--- contracts/RequestParkingSpotToken.sol
+++ contracts/RequestParkingSpotToken.sol
@@ -1,10 +1,22 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+interface ParkingSpotAttributes {
+    function checkSpotAvailability(uint) external view returns (bool);
+}
+
+interface ParkingSpotToken {
+    function ownerOf(uint256) external returns (address);
+    function safeTransferFrom(address,address,uint256) external;
+
+}
 
 contract RequestParkingSpotToken {
 
     mapping(address=>uint256) public depositors;
+
+    ParkingSpotAttributes constant psa = ParkingSpotAttributes(0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9);
+    ParkingSpotToken constant pst = ParkingSpotToken(0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9);
 
 
     // Payable address can receive Ether
@@ -22,10 +34,6 @@ contract RequestParkingSpotToken {
         depositors[msg.sender] += msg.value;
     }
 
-    // Call this function along with some Ether.
-    // The function will throw an error since this function is not payable.
-    function notPayable() public {}
-
     // Function to withdraw all Ether from this contract.
     function withdraw(uint256 _amount) public {
         require(_amount <= depositors[msg.sender], "Not enough ETH deposited");
@@ -35,10 +43,15 @@ contract RequestParkingSpotToken {
 
     }
 
-    // Function to transfer Ether from this contract to address from input
-    function transfer(address payable _to, uint _amount) public {
-        // Note that "to" is declared as payable
-        (bool success, ) = _to.call{value: _amount}("");
-        require(success, "Failed to send Ether");
+
+    function requestParkingSpotToken(uint256 _tokenId) public {
+        require(depositors[msg.sender] >= 1000000000000000000, "Must deposit at least 1 Eth");
+        require(psa.checkSpotAvailability(_tokenId) == true, "Parking spot is unavailable!");
+
+        address currentOwner;
+        currentOwner = pst.ownerOf(_tokenId);
+        pst.safeTransferFrom(currentOwner, msg.sender, _tokenId);
+
     }
+
 }
